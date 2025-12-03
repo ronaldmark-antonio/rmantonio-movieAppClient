3f2cf7804df5f3532bfd2171462320eb1c44f1ca
ronaldmark.antonio <ronaldmark.antonio@gmail.com>
2025-12-03 23:00:34 +0800

    Restore UserView.js in components

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Register from './pages/Register';
 import Movies from './pages/Movies';
 import UserProvider from './UserContext';
 import AdminView from './components/AdminView';
+import UserView from './components/UserView';
 import ViewMovie from './components/ViewMovie';
 
 function App() {
